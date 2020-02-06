f2777b9bdc5731fb5173824ad5fad3dce2010c7d
Felipe Cardoso Coelho <felipe.coelho@pampa.compasso>
2020-02-06 20:23:51 -0300

    Removendo ifs desnecessarios

--- gerenciarArquivos.py
+++ gerenciarArquivos.py
@@ -94,18 +94,13 @@ arqResultado.write(str(numReprovados)+' REPROVADO(S)\n')
 
 
 for j in range(25):
+    if j == 0:
+        arqResultado.write('      ')
     if vetorAcertadas[j] < 10:
-        if j == 0:
-            arqResultado.write('      ')
         arqResultado.write('0' + str(vetorAcertadas[j]) + str(vetorErradas[j])+' ')
-        j+=1
     elif vetorErradas[j] < 10:
-        if j == 0:
-            arqResultado.write('      ')
         arqResultado.write(str(vetorAcertadas[j]) + '0' + str(vetorErradas[j])+' ')
     else:
-        if j == 0:
-            arqResultado.write('      ')
         arqResultado.write(str(vetorAcertadas[j]) + str(vetorErradas[j])+' ')
     
     
